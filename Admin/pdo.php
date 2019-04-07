fbe0788da58cdddd606f6808f983e470fa9ae4a4
Cvijetin Vidaković <vidakovic.cvijetin@gmail.com>
2019-04-07 12:39:14 +0200

    Update pdo.php

--- Admin/pdo.php
+++ Admin/pdo.php
@@ -1,6 +1,6 @@
 <?php 
 try {
-	$db = new PDO('mysql:host=localhost;dbname=infodaska2019', 'infodaska7', 'UHduwhd.ojwf');
+	$db = new PDO('mysql:host=localhost;dbname=infodaska2019', '', '');
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 	$db->setAttribute(PDO::MYSQL_ATTR_USE_BUFFERED_QUERY, true);
 } catch(PDOException $e) {
